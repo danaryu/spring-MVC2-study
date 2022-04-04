1a1f472564af5f45b83a025ac7dd3c79ee7f2033
danaRyu <dana40426@gmail.com>
2022-04-04 22:35:23 +0900

    DefaultMessageCodeResolver

---
+++ validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,40 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodeResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+
+//        new ObjectError("item", new String[]{"required.item", "required"});
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String code : messageCodes) {
+            System.out.println("code = " + code);
+        }
+
+        assertThat(messageCodes).containsExactly("required.item.itemName"
+            , "required.itemName"
+            , "required.java.lang.String"
+            , "required");
+        //BindingResult.rejectValue가 내부적으로 MessageCodeResolver를 호출 -> new FieldError("item", "itemName", null, false, messageCodes, null, null);
+    }
+
+}
